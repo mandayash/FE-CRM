0be7d1bd972465efc96f80b25bb709dec6ec237b
Ahmad Naufal Muzakki <naufalmuzakki895@gmail.com>
2025-04-01 14:48:30 +0700

    try to now where's the issue

--- lib/api.ts
+++ lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getCookie } from "cookies-next";
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
